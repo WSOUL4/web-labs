d14eb815b539f7c30f8c890776c103988dbe19d9
WSOUL4 <mitrinov4124@gmail.com>
2025-03-02 12:30:13 +0400

    featutre LAB1-2 Подключен базовый сервер на express
    в  app.js  запуск сервера по конфигурации из .env проекта.
    Добавлен middleware - morgan для логирования

---
+++ backend/Database/db_start.js
@@ -0,0 +1,5 @@
+import sequelize from 'sequelize';
+import dotenv from 'dotenv';
+import pg from 'pg';
+
+dotenv.config()
---
+++ backend/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+import logger from 'morgan'
+
+function appServerLaunch(){
+    const app = express();
+    app.use(cors())//middleware
+    app.use(express.json())//middleware
+    dotenv.config()//Для чтения из .env
+    const port = process.env.PORT;
+    app.listen(port, (error) => {
+        if (error) console.error('Не удалось запустить сервер:', error);
+        console.info(`Сервер запустился на порте ${port}`);
+    });
+    return app;
+}
+
+let app=appServerLaunch();
+app.use(logger('dev'));
