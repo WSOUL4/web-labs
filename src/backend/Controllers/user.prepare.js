26c4b77f757c16c15f65ccafb2fe188351a20385
WSOUL4 <mitrinov4124@gmail.com>
2025-03-14 20:12:44 +0400

    bugfix LAB3

---
+++ src/backend/Controllers/user.prepare.js
@@ -0,0 +1,38 @@
+import express from "express";
+//import {getParameterByName} from './SharedFuncs.js'
+import { valErr } from "../CustomErrors/errors.js";
+import { User } from "../Models/User/user.model.js";
+import { conn } from "../Configs/start.database.js";
+import { isUniqueEmail, isUniqueId } from "../Utilities/Users/user.utils.js";
+
+function checkRequiredField(req, res, next) {
+  //console.log(req.body);
+  let email = req.body.email;
+  if (!email) {
+    valErr(res);
+    return;
+  }
+
+  //console.log(id);
+
+  isUniqueEmail(email)
+    .then((isEmailUni) => {
+      if (isEmailUni) {
+        // console.log('GOOD');
+
+        next();
+      } else {
+        valErr(res);
+        //res.status(400).send({message:'Not unique email  or id'});
+        //next();
+        //return;
+      }
+    })
+
+    .catch((err) => {
+      console.log(err);
+
+      next(err);
+    });
+}
+export { checkRequiredField };
---
+++ src/backend/Models/Event/event.model.js
@@ -0,0 +1,31 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { conn } from "../../Configs/start.database.js";
+import { User } from "../User/user.model.js";
+const Event = conn.define(
+  "Event",
+  {
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    date: DataTypes.DATEONLY,
+    createdBy: { type: DataTypes.INTEGER, foreignKey: true }, //, foreignKey: true
+    //id: {type: DataTypes.INTEGER, primaryKey: true},
+  },
+  {
+    // don't add the timestamp attributes (updatedAt, createdAt)
+    timestamps: false,
+  },
+);
+User.hasMany(Event, {
+  foreignKey: "createdBy",
+});
+Event.belongsTo(User, {
+  foreignKey: "createdBy",
+});
+async function syncModels() {
+  await Event.sync({ alter: true }); // Обратите внимание: force: true удалит таблицы перед созданием
+  console.log("Tables synced!");
+}
+
+syncModels().catch((err) => console.error(err));
+
+export { Event };
